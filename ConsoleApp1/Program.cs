bd723cb78ed8c42592f0e083717064fb07a414b8
wiktormuller <biznes.wiktor@gmail.com>
2020-03-17 16:38:16 +0100

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("---------- MathSolver ----------");
+            Console.Write("Chose one of 10 options: ");
+            var option = Console.ReadKey();
+
+            UserInterface.ChoseOption(option);
+        }
+    }
+}
---
+++ ConsoleApp1/UserInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public static class UserInterface
+    {
+        public static void ChoseOption(int option)
+        {
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("Find Min Value from sequence: ");
+                    break;
+            }
+        }
+    }
+}
